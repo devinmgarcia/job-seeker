a60f3985cdf6eb00a7db6026aa75669a47cc2ee1
Devin Garcia <devinmgarcia@gmail.com>
2023-11-28 16:40:05 -0500

    made first react native app

---
+++ utils/checkImageUrl.js
@@ -0,0 +1,11 @@
+const checkImageURL = (url) => {
+  if (!url) return false;
+  else {
+    const pattern = new RegExp(
+      "^https?://.+.(png|jpg|jpeg|bmp|gif|webp)$",
+      "i"
+    );
+    return pattern.test(url);
+  }
+};
+export default checkImageURL;
